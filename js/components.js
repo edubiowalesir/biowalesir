e7de4c800739abf168de4a4eaff3f5f155f54a6e
BioWaleSir <edu.biowalesir@gmail.com>
2026-04-11 22:45:04 +0530

    Update components.js

--- js/components.js
+++ js/components.js
@@ -20,7 +20,7 @@ const NAV_HTML = `
 <a href="/biowalesir/index.html#about">ℹ️ About Us</a>
 <a href="/biowalesir/index.html#contact">📬 Contact</a>
 <a href="/biowalesir/index.html#donate">💛 Donate</a>
-<a> href="https://edubiowalesir.github.io/pathpedia">PathoPedia</a>
+<a> href="https://edubiowalesir.github.io/pathpedia">🦠 PathoPedia</a>
 </div>
     </li>
   </ul>
