eae3dee90ae96656052eb80b680a3256a72e3015
BioWaleSir <edu.biowalesir@gmail.com>
2026-05-01 13:31:31 +0530

    Update components.js

--- js/components.js
+++ js/components.js
@@ -3,23 +3,23 @@
 
 const NAV_HTML = `
 <nav class="navbar">
-  <a href="/biowalesir/index.html" class="nav-logo">
+  <a href="/index.html" class="nav-logo">
   <span class="leaf">🌿</span>
   BioWaleSir
 </a>
   <ul class="nav-links" id="navLinks">
-    <li><a href="/biowalesir/index.html">Home</a></li>
-<li class="nav-class"><a href="/biowalesir/class9.html">Class 9</a></li>
-<li class="nav-class"><a href="/biowalesir/class10.html">Class 10</a></li>
-<li class="nav-class"><a href="/biowalesir/class11.html">Class 11</a></li>
-<li class="nav-class"><a href="/biowalesir/class12.html">Class 12</a></li>
+    <li><a href="/index.html">Home</a></li>
+<li class="nav-class"><a href="/class9.html">Class 9</a></li>
+<li class="nav-class"><a href="/class10.html">Class 10</a></li>
+<li class="nav-class"><a href="/class11.html">Class 11</a></li>
+<li class="nav-class"><a href="/class12.html">Class 12</a></li>
     <li class="nav-more">
       <button class="nav-more-btn">More</button>
       <div class="nav-dropdown">
-       <a href="/biowalesir/teacher.html">👩‍🏫 Teacher Section</a>
-<a href="/biowalesir/index.html#about">ℹ️ About Us</a>
-<a href="/biowalesir/index.html#contact">📬 Contact</a>
-<a href="/biowalesir/index.html#donate">💛 Donate</a>
+       <a href="/teacher.html">👩‍🏫 Teacher Section</a>
+<a href="/index.html#about">ℹ️ About Us</a>
+<a href="/index.html#contact">📬 Contact</a>
+<a href="/index.html#donate">💛 Donate</a>
 <a href="https://edubiowalesir.github.io/pathpedia/index.html">🦠 PathoPedia</a>
 </div>
     </li>
@@ -29,17 +29,17 @@ const NAV_HTML = `
   </button>
 </nav>
 <div class="mobile-nav" id="mobileNav">
- <a href="/biowalesir/index.html">🏠 Home</a>
+ <a href="/index.html">🏠 Home</a>
   <div class="mobile-divider"></div>
-  <a href="/biowalesir/class9.html">📗 Class 9</a>
-<a href="/biowalesir/class10.html">📘 Class 10</a>
-<a href="/biowalesir/class11.html">📙 Class 11</a>
-<a href="/biowalesir/class12.html">📕 Class 12</a>
+  <a href="/class9.html">📗 Class 9</a>
+<a href="/class10.html">📘 Class 10</a>
+<a href="/class11.html">📙 Class 11</a>
+<a href="/class12.html">📕 Class 12</a>
   <div class="mobile-divider"></div>
-  <a href="/biowalesir/teacher.html">👩‍🏫 Teacher Section</a>
-<a href="/biowalesir/index.html#about">ℹ️ About Us</a>
-<a href="/biowalesir/index.html#contact">📬 Contact</a>
-<a href="/biowalesir/index.html#donate">💛 Donate</a>
+  <a href="/teacher.html">👩‍🏫 Teacher Section</a>
+<a href="/index.html#about">ℹ️ About Us</a>
+<a href="/index.html#contact">📬 Contact</a>
+<a href="/index.html#donate">💛 Donate</a>
 <a href="https://edubiowalesir.github.io/pathpedia/index.html">🦠 PathoPedia</a>
 </div>
 `;
@@ -54,10 +54,10 @@ const FOOTER_HTML = `
     <div class="footer-col">
       <h4>Classes</h4>
       <ul>
-        <li><a href="/biowalesir/class9.html">Class 9 Biology</a></li>
-<li><a href="/biowalesir/class10.html">Class 10 Biology</a></li>
-<li><a href="/biowalesir/class11.html">Class 11 Biology</a></li>
-<li><a href="/biowalesir/class12.html">Class 12 Biology</a></li>
+        <li><a href="/class9.html">Class 9 Biology</a></li>
+<li><a href="/class10.html">Class 10 Biology</a></li>
+<li><a href="/class11.html">Class 11 Biology</a></li>
+<li><a href="/class12.html">Class 12 Biology</a></li>
       </ul>
     </div>
     <div class="footer-col">
@@ -67,7 +67,7 @@ const FOOTER_HTML = `
         <li><a href="#">PYQ Papers</a></li>
         <li><a href="#">Diagrams</a></li>
         <li><a href="#">Mind Maps</a></li>
-        <li><a href="/biowalesir/teacher.html">For Teachers</a></li>
+        <li><a href="/teacher.html">For Teachers</a></li>
       </ul>
     </div>
     <div class="footer-col">
